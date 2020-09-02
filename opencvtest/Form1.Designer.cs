fb20803af99f25f251ddb82207313595aaf7813d
ysh3436 <ysh3436@gmail.com>
2020-09-02 16:31:55 +0900

    OpenCV Mouse Macro Test Program
    OpenCV Mouse Macro Test Program

---
+++ opencvtest/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace opencvtest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonSearch = new System.Windows.Forms.Button();
+            this.pictureBox_reference = new System.Windows.Forms.PictureBox();
+            this.textBox_reference = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.buttonReset = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_reference)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonSearch
+            // 
+            this.buttonSearch.Location = new System.Drawing.Point(251, 12);
+            this.buttonSearch.Name = "buttonSearch";
+            this.buttonSearch.Size = new System.Drawing.Size(93, 38);
+            this.buttonSearch.TabIndex = 0;
+            this.buttonSearch.Text = "Search";
+            this.buttonSearch.UseVisualStyleBackColor = true;
+            this.buttonSearch.Click += new System.EventHandler(this.buttonSearch_Click);
+            // 
+            // pictureBox_reference
+            // 
+            this.pictureBox_reference.BackColor = System.Drawing.SystemColors.Window;
+            this.pictureBox_reference.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox_reference.ImageLocation = "";
+            this.pictureBox_reference.InitialImage = null;
+            this.pictureBox_reference.Location = new System.Drawing.Point(25, 66);
+            this.pictureBox_reference.Name = "pictureBox_reference";
+            this.pictureBox_reference.Size = new System.Drawing.Size(75, 75);
+            this.pictureBox_reference.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox_reference.TabIndex = 4;
+            this.pictureBox_reference.TabStop = false;
+            this.pictureBox_reference.Visible = false;
+            this.pictureBox_reference.Click += new System.EventHandler(this.pictureBox_reference_Click);
+            // 
+            // textBox_reference
+            // 
+            this.textBox_reference.Location = new System.Drawing.Point(25, 153);
+            this.textBox_reference.Name = "textBox_reference";
+            this.textBox_reference.ReadOnly = true;
+            this.textBox_reference.Size = new System.Drawing.Size(76, 21);
+            this.textBox_reference.TabIndex = 5;
+            this.textBox_reference.Visible = false;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(25, 22);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(217, 20);
+            this.comboBox1.TabIndex = 6;
+            // 
+            // buttonReset
+            // 
+            this.buttonReset.Location = new System.Drawing.Point(350, 12);
+            this.buttonReset.Name = "buttonReset";
+            this.buttonReset.Size = new System.Drawing.Size(93, 38);
+            this.buttonReset.TabIndex = 0;
+            this.buttonReset.Text = "Reset";
+            this.buttonReset.UseVisualStyleBackColor = true;
+            this.buttonReset.Click += new System.EventHandler(this.buttonReset_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.Location = new System.Drawing.Point(25, 191);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(715, 4);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Visible = false;
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(469, 21);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(77, 12);
+            this.linkLabel1.TabIndex = 7;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Img_License";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(766, 207);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox_reference);
+            this.Controls.Add(this.pictureBox_reference);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.buttonReset);
+            this.Controls.Add(this.buttonSearch);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_reference)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonSearch;
+        private System.Windows.Forms.PictureBox pictureBox_reference;
+        private System.Windows.Forms.TextBox textBox_reference;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button buttonReset;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+    }
+}
+
---
+++ opencvtest/Form1.cs
@@ -0,0 +1,148 @@
+﻿using OpenCvSharp;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace opencvtest
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+        public static extern void mouse_event(uint dwFlags, uint dx, uint dy, uint cButtons, uint dwExtraInfo);
+        [DllImport("user32.dll")]
+        static extern bool SetCursorPos(int x, int y);
+
+        private const int MOUSEEVENTF_LEFTDOWN = 0x02;
+        private const int MOUSEEVENTF_LEFTUP = 0x04;
+        private const int MOUSEEVENTF_RIGHTDOWN = 0x08;
+        private const int MOUSEEVENTF_RIGHTUP = 0x10;
+
+        //String AppPlayerName = "Whale";
+        Screen[] scr = Screen.AllScreens;
+        int iClicked = 0;
+        Bitmap Stone_Img = null;
+        int[] count;
+        PictureBox[] pictureBoxArray;
+        TextBox[] textBoxArray;
+        public Form1()
+        {
+            InitializeComponent();
+
+            DirectoryInfo macroFileDirectory = new DirectoryInfo(Application.StartupPath + "\\Img");
+            if (macroFileDirectory.Exists == false)
+            {
+                macroFileDirectory.Create();
+            }
+            foreach (var item in macroFileDirectory.GetFiles("*.png"))
+            {
+                comboBox1.Items.Add(item.ToString());
+            }
+            pictureBoxArray = new PictureBox[comboBox1.Items.Count];
+            textBoxArray = new TextBox[comboBox1.Items.Count];
+            count = new int[comboBox1.Items.Count];
+            for (int i = 0; i < comboBox1.Items.Count; i++)
+            {
+                count[i] = 0;
+                pictureBoxArray[i] = new PictureBox();
+                pictureBoxArray[i].Location = new System.Drawing.Point(this.pictureBox_reference.Location.X + (90 * i), this.pictureBox_reference.Location.Y);
+                pictureBoxArray[i].Name = "pictureBox_" + i.ToString();
+                pictureBoxArray[i].Size = this.pictureBox_reference.Size;
+                pictureBoxArray[i].SizeMode = pictureBox_reference.SizeMode;
+                pictureBoxArray[i].Text = pictureBox_reference.Text;
+                pictureBoxArray[i].Tag = i;
+                pictureBoxArray[i].BackColor = Color.White;
+                pictureBoxArray[i].Click += new System.EventHandler(this.pictureBox_reference_Click);
+                Bitmap bmp;
+                bmp = new Bitmap(Application.StartupPath + "\\Img\\" + comboBox1.Items[i].ToString());
+                pictureBoxArray[i].Image = (Image)bmp;
+
+                textBoxArray[i] = new TextBox();
+                textBoxArray[i].Location = new System.Drawing.Point(this.textBox_reference.Location.X + (90 * i), this.textBox_reference.Location.Y);
+                textBoxArray[i].Margin = this.textBox_reference.Margin;
+                textBoxArray[i].Name = "pictureBox_" + i.ToString();
+                textBoxArray[i].Size = this.textBox_reference.Size;
+                textBoxArray[i].Text = 0.ToString();
+                textBoxArray[i].TextAlign = HorizontalAlignment.Center;
+                textBoxArray[i].Tag = i;
+
+                this.Controls.Add(pictureBoxArray[i]);
+                this.Controls.Add(textBoxArray[i]);
+            }
+        }        
+        public void searchIMG(Bitmap screen_img, Bitmap find_img)
+        {
+            //스크린 이미지 선언
+            using (Mat ScreenMat = OpenCvSharp.Extensions.BitmapConverter.ToMat(screen_img))
+            //찾을 이미지 선언
+            using (Mat FindMat = OpenCvSharp.Extensions.BitmapConverter.ToMat(find_img))
+            //스크린 이미지에서 FindMat 이미지를 찾아라
+            using (Mat res = ScreenMat.MatchTemplate(FindMat, TemplateMatchModes.CCoeffNormed))
+            {
+                //찾은 이미지의 유사도를 담을 더블형 최대 최소 값을 선언합니다.
+                double minval, maxval = 0;
+                //찾은 이미지의 위치를 담을 포인트형을 선업합니다.
+                OpenCvSharp.Point minloc, maxloc;
+                //찾은 이미지의 유사도 및 위치 값을 받습니다. 
+                Cv2.MinMaxLoc(res, out minval, out maxval, out minloc, out maxloc);
+                Debug.WriteLine("찾은 이미지의 유사도 : " + maxval);
+
+                if (maxval >= 0.9)
+                {
+                    OpenCvSharp.Point clickpoint = new OpenCvSharp.Point(maxloc.X + FindMat.Width / 2, maxloc.Y + FindMat.Height / 2);
+                    MacroMouseClick(clickpoint);                    
+                }
+
+            }
+        }
+        private void MacroMouseClick(OpenCvSharp.Point savePoint)
+        {
+            SetCursorPos(savePoint.X, savePoint.Y);
+            mouse_event(MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+        }
+
+        private void pictureBox_reference_Click(object sender, EventArgs e)
+        {
+            var senderPictureBox = sender as PictureBox;
+            count[(int)senderPictureBox.Tag] = count[(int)senderPictureBox.Tag] + 1;
+            textBoxArray[(int)senderPictureBox.Tag].Text = count[(int)senderPictureBox.Tag].ToString();
+        }
+
+        private void buttonReset_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < count.Length; i++)
+            {
+                count[i] = 0;
+                textBoxArray[i].Text = count[i].ToString();
+            }
+        }
+
+        private void buttonSearch_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.Text == "") return;
+            Stone_Img = new Bitmap(Application.StartupPath + "\\Img\\" + comboBox1.Text);
+            //플레이어 창 크기 만큼의 비트맵을 선언해줍니다.
+            Bitmap bmp = new Bitmap(scr[0].Bounds.Width, scr[0].Bounds.Height);
+            var captureimg = Graphics.FromImage(bmp);
+            captureimg.CopyFromScreen(scr[0].Bounds.X, scr[0].Bounds.Y, 0, 0, scr[0].Bounds.Size);
+
+            // pictureBox1 이미지를 표시해줍니다.
+            pictureBox1.Image = bmp;
+
+            searchIMG(bmp, Stone_Img);
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start(@"iexplore.exe", Application.StartupPath + "\\Img\\license.html");
+        }
+    }
+}
